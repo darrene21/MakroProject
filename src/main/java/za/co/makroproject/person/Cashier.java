1c2786d9e638bf0b36b95add722ee86690ce97fb
darrene21 <nene07013@gmail.com>
2019-11-01 14:57:31 +0200

    changes made

--- src/main/java/za/co/makroproject/person/Cashier.java
+++ src/main/java/za/co/makroproject/person/Cashier.java
@@ -1,8 +1,30 @@
-class Cashier extends Employee{
+package za.co.makroproject.person.Cashier;
+import java.util.Scanner;
 
-public void login(){
+public class Cashier extends Employee{
+
+private double amountReceived;
+private double change;
+
+Cashier(){}
+Cashier(double amountReceipt,double change){
+Super(employeeNumber,username,passord,position);
+
+this.amountReceived = amountReceived;
+this.change = change;
+
 }
-public void register(){
+
+Scanner sc = new Scanner(System.in);
+
+public void giveChange(){
+System.out.println("Enter amount");
+double amount = sc.nextDouble();
+
+
+//public void scanProduct(){
+//System.out.println("Enter bar code ");
+//String barCode = sc.nextLine;
 }
-public void givesChange(){
 }
+
