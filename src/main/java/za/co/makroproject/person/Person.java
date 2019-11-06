ec3f35cb224d13cfacc1fe1828e9ad46fbe4a952
bigked <bigked14@gmail.com>
2019-11-06 12:13:17 +0200

    go

--- src/main/java/za/co/makroproject/person/Person.java
+++ src/main/java/za/co/makroproject/person/Person.java
@@ -1,49 +1,94 @@
-package za.co.makroproject.person;
+package za.co.makroproject.person.Person;
 public abstract class Person{
 
 private String firstName;
-private String lastName;
-private String dateOfBirth;
+private String surname;
+private String dob;
 private long phoneNumber;
+private long idNumber;
 private String homeAddress;
 private String emailAddress;
+private String gender;
+private String maritalStatus;
 
 public void setFirstName(String firstName){
 this.firstName = firstName;
 }
+
 public String getFirstName(){
 return firstName;
 }
-public void setLastName(String lastName){
-this.lastName = lastName;
+
+public void setSurname(String surname){
+this.surname = surname;
 }
-public String getLastName(){
-return lastName;
+
+public String getSurname(){
+return surname;
 }
-public void setDateOfBirth(String dateOfBirth){
-this.dateOfBirth = dateOfBirth;
+
+public void setDob(String dob){
+this.dob = dob;
+
 }
-public String getDateOfBirth(){
-return dateOfBirth;
+
+public String getDob(){
+return dob;
 } 
+
 public void setPhoneNumber(long phoneNumber){
 this.phoneNumber = phoneNumber;
 }
-public long getPhonenumber(){
+
+public long getPhoneNumber(){
 return phoneNumber;
 }
+
+public void setIdNumber(long idNumber){
+this.idNumber = idNumber;
+}
+
+public long getIdNumber(){
+return idNumber;
+}
+
 public void setHomeAdress(String homeAddress){
 this.homeAddress = homeAddress;
 }
+
 public String getHomeAddress(){
 return homeAddress;
 }
+
 public void setEmailAddress(String emailAddress){
 this.emailAddress = emailAddress;
 }
+
 public String getEmailAddress(){
 return emailAddress;
 }
+
+public void setGender(String gender){
+this.gender = gender;
+}
+
+public String getGender(){
+return gender;
+}
+
+public void setMaritalStatus(String maritalStatus){
+this.maritalStatus = maritalStatus;
+}
+
+public String getMaritalStatus(){
+return maritalStatus;
+}
+
+public String toString() {
+  return "Person [firstName=" + firstName + ", surname=" + surname + ", dob=" + dob + ", phoneNumber=" + phoneNumber + ", idNumber=" + idNumber + ", homeAddress=" + homeAddress + ", emailAddress=" + emailAddress + ", gender=" + gender + ", maritalStatus=" + maritalStatus + "]";
+
+}
+
 }
 
 
